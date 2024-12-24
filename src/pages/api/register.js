d39f3b4136479109c65b7798ac531e22fa610e40
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-24 14:05:23 +0100

    Feat: add  validation email if

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -1,18 +1,22 @@
-import dbConnect from "@/config/mongodb";
-import User from "@/models/User";
+import dbConnect from "pages/config/mongodb";
+import User from "pages/models/User";
 import bcrypt from "bcryptjs";
 
 export default async function register(req, res) {
     if (req.method === "POST") {
         await dbConnect();
         const { fullName, email, password } = req.body;
-        
         if (!fullName || !email || !password) {
             return res.status(400).json({ success: false, message: "Tous les champs sont requis" });
         }
-        
+
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ success: false, message: "this email already exist" });
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10);
-        
         try {
             const user = await User.create({ fullName, email, password: hashedPassword });
             res.status(201).json({ success: true, user });
