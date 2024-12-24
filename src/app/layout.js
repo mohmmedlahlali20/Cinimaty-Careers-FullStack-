96f317f165012dfced7d827e990dda39930e2ecb
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-24 22:35:04 +0100

    refactor: mise à jour de la mise en page de l'application

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <section>
+        <section className="">
           {children}
         </section>
       </body>
