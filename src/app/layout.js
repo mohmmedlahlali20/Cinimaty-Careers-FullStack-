df5a14de09c203f699091c38066f8e7e5dd9f758
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-25 11:14:25 +0100

    refactor: ajustements dans la mise en page globale

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Providers from "global/provider";
+import Providers from "./global/provider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
