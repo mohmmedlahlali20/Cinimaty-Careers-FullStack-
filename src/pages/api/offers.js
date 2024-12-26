530066173f84dd42a03e4b3fde069eec7bf6d913
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-26 15:45:25 +0100

    fix: corrections sur l'API des offres

--- src/pages/api/offers.js
+++ src/pages/api/offers.js
@@ -1,23 +1,15 @@
 import nestApi from "app/utils/pathNest";
 
-export async function getServerSideProps() {
-    try {
-        const response = await nestApi.get('/offers');
-        const offers = response.data;
-        console.log(offers);
-        
-        return {
-            props: {
-                offers, 
-            },
-        };
-    } catch (error) {
-        console.error('Erreur lors de la récupération des offres:', error.message);
-        return {
-            props: {
-                offers: [],
-                error: 'Erreur lors de la récupération des données',
-            },
-        };
-    }
+export default async function handler(req, res) {
+  const { query } = req.query;
+
+  try {
+    const response = await nestApi.get(`/offers?q=${query}`);
+    const offers = response.data;
+
+    res.status(200).json(offers);
+  } catch (error) {
+    console.error("Error fetching offers:", error);
+    res.status(500).json({ error: "Failed to fetch offers" });
+  }
 }
