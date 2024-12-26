c97099e9633a6f0e14c3dfdbe520bef4afd147a9
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-26 09:32:31 +0100

    refactor: mise à jour du store Redux

--- src/app/_store/store.js
+++ src/app/_store/store.js
@@ -1,11 +1,13 @@
 'use client'
 
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from './slice/authSlice'
+import authReducer from './slice/authSlice';
+import offersReducer from './slice/authSlice'
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
+        offers: offersReducer,
     },
 });
 
