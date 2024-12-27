0265cb6d1a189dc286b4db395e284e9463f7122a
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-27 10:51:23 +0100

    fix: mise à jour de la barre de recherche

--- src/app/offers/components/searchBar.jsx
+++ src/app/offers/components/searchBar.jsx
@@ -1,17 +1,17 @@
-'use client'
+'use client';
 
-import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 
 const SearchBar = () => {
-    const [query, setQuery] = useState('')
-    const router = useRouter()
+    const [query, setQuery] = useState('');
+    const router = useRouter();
 
     const handleSearch = (e) => {
-        e.preventDefault()
-        const encodedQuery = encodeURIComponent(query)
-        router.push(`/?query=${encodedQuery}`)
-    }
+        e.preventDefault();
+        const Query = encodeURIComponent(query);
+        router.push(`/offers/?query=${Query}`);
+    };
 
     return (
         <form onSubmit={handleSearch} className="m-8 flex justify-center">
@@ -29,14 +29,12 @@ const SearchBar = () => {
                             clipRule="evenodd"
                         />
                     </svg>
-
                     <input
                         className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
                         placeholder="UI Kits, Dashboards..."
                         value={query}
                         onChange={(e) => setQuery(e.target.value)}
                     />
-
                     <button
                         className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
                         type="submit"
@@ -46,8 +44,7 @@ const SearchBar = () => {
                 </div>
             </div>
         </form>
-    )
-}
+    );
+};
 
-export default SearchBar
-
+export default SearchBar;
