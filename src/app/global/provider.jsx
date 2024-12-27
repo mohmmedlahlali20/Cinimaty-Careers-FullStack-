24153de0100eacf393f8a682580d8f2257b1ccd5
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-27 10:51:36 +0100

    chore: suppression de l'ancien provider global

--- src/app/global/provider.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import store from "app/_store/store";
-import { Provider } from "react-redux";
-
-
-export default function Providers({ children }) {
-    return <Provider store={store}>{children}</Provider>;
-}
