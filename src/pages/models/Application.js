4362f497d3951c2d5bb51b348f55a71b16903078
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-28 22:01:08 +0100

    feat: ajout du modèle Application

---
+++ src/pages/models/Application.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const ApplicationSchema = new mongoose.Schema({
+    nom:
+    {
+        type: String,
+        required: true
+    },
+    prenom:
+    {
+        type: String,
+        required: true
+    },
+    cv:
+    {
+        type: String,
+        required: true
+    },
+    letterCover:
+    {
+        type: String,
+        required: true
+    },
+    numeroTelephone:
+    {
+        type: String,
+        required: true
+    },
+    localisation:
+    {
+        type: String,
+        required: true
+    },
+    dateDisponibilite:
+    {
+        type: Date,
+        required: true
+    },
+    offerId:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Offer',
+        required: true
+    }
+
+}, { timestamps: true });
+
+export default mongoose.models.Application || mongoose.model('Application', ApplicationSchema);
