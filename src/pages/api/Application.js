88c044d4023badc396462efd5de835b15a6039a9
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-28 22:00:57 +0100

    feat: ajout de l'API pour les applications

---
+++ src/pages/api/Application.js
@@ -0,0 +1,97 @@
+import multer from 'multer';
+import dbConnect from 'pages/config/mongodb';
+import Application from 'pages/models/Application';
+import cloudinary from 'pages/config/cloudinary';
+import { parse } from 'date-fns';
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+async function uploadToCloudinary(fileBuffer, options) {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(options, (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+    stream.end(fileBuffer);
+  });
+}
+
+export default async function Postuler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ success: false, message: 'Méthode non autorisée.' });
+  }
+
+  const multerMiddleware = upload.single('cv');
+  await new Promise((resolve, reject) => {
+    multerMiddleware(req, res, (err) => {
+      if (err) reject(err);
+      resolve();
+    });
+  });
+
+  await dbConnect();
+
+  try {
+    const {
+      nom,
+      prenom,
+      letterCover,
+      numeroTelephone,
+      localisation,
+      dateDisponibilite,
+      offerId,
+    } = req.body;
+
+    const cvFile = req.file;
+
+    if (
+        !nom ||
+        !prenom ||
+        !cvFile ||
+        !letterCover ||
+        !numeroTelephone ||
+        !localisation ||
+        !dateDisponibilite ||
+        !offerId
+    ) {
+      return res.status(400).json({ success: false, message: 'Tous les champs sont requis.' });
+    }
+
+    const parsedDate = parse(dateDisponibilite, 'dd/MM/yyyy', new Date());
+    if (isNaN(parsedDate)) {
+      return res
+          .status(400)
+          .json({ success: false, message: 'La date de disponibilité est invalide.' });
+    }
+    const uploadResult = await uploadToCloudinary(cvFile.buffer, {
+      folder: 'job-applications',
+      resource_type: 'raw',
+      public_id: `${nom}_${prenom}_CV`,
+      use_filename: true,
+    });
+
+    const cvUrl = uploadResult.secure_url;
+
+    const application = await Application.create({
+      nom,
+      prenom,
+      cv: cvUrl,
+      letterCover,
+      numeroTelephone,
+      localisation,
+      dateDisponibilite: parsedDate,
+      offerId,
+    });
+
+    res.status(201).json({ success: true, data: application });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Erreur serveur.' });
+  }
+}
