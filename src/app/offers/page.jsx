01de80229fe65e0af0984e0dfcaa0ca95249a5ab
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-31 10:53:33 +0100

    fix: améliorations diverses sur la page des offres

--- src/app/offers/page.jsx
+++ src/app/offers/page.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Offers from './components/Offers';
 
 
 export default function Home({ searchParams }) {
-  return <Offers searchParams={searchParams} />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Offers searchParams={searchParams} />
+    </Suspense>
+  )
 }
