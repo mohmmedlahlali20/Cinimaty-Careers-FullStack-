0069c70256f80c0fc8972f7cf0ec3e2dd08658ea
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-31 11:47:49 +0100

    feat(layout): update global layout structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 
 const geistSans = Geist({
@@ -23,9 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <section className="">
+        <main className="">
           {children}
-        </section>
+        </main>
       </body>
     </html>
   );
