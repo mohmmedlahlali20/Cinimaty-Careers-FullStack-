0f8bea50035e4feec575678a8aafc958d4d3dec7
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-03 21:22:09 +0100

    Update mongodb.js configuration for improved connection setup

--- src/pages/config/mongodb.js
+++ src/pages/config/mongodb.js
@@ -12,19 +12,13 @@
     }
 
     async function dbConnect() {
-    if (cached.conn) {
-        return cached.conn;
-    }
-    if (!cached.promise) {
-        cached.promise = mongoose
-        .connect(MONGODB_URI, {
+        if (mongoose.connections[0].readyState) {
+            return mongoose.connections[0];
+        }
+        return mongoose.connect(MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        })
-        .then((mongoose) => mongoose);
-    }
-    cached.conn = await cached.promise;
-    return cached.conn;
+        });
     }
 
     export default dbConnect;
