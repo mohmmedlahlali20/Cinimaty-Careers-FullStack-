76e7fdd9ff69d87e3c505ea7ecf78e8b310fd9b8
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-05 17:31:35 +0100

    chore: mise à jour des packages et des configurations Swagger

--- src/pages/api/swagger.js
+++ src/pages/api/swagger.js
@@ -1,19 +1,6 @@
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../config/swagger");
+// pages/api/swagger.js
+import swaggerDocument from "../swagger"
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default async function handler(req, res) {
-  if (req.method === "GET") {
-    const html = swaggerUi.generateHTML(swaggerDocument);
-    res.setHeader("Content-Type", "text/html");
-    res.status(200).send(html);
-  } else {
-    res.setHeader("Allow", ["GET"]);
-    res.status(405).end(`Méthode ${req.method} non autorisée`);
-  }
+export default function handler(req, res) {
+    res.status(200).json(swaggerDocument);
 }
