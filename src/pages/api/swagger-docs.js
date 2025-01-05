9a1d4337d891f04caca6677f13a0b348e18080bd
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-05 17:31:38 +0100

    chore: suppression des anciens fichiers Swagger obsolètes

--- src/pages/api/swagger-docs.js
+++
@@ -1,8 +0,0 @@
-const swaggerUi = require("swagger-ui-express");
-import swaggerDocument from "../config/swagger";
-
-const setupSwagger = swaggerUi.setup(swaggerDocument);
-
-export default function handler(req, res) {
-  swaggerUi.serve(req, res, () => setupSwagger(req, res));
-}
